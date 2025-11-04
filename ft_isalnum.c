03ba462bb1f1d6a44c8a30194d87b89616cae751
Maxence De Carvalho <mde-carv@c3r3p6.42perpignan.fr>
2025-11-04 10:21:07 +0100

    isalpha to strlen

---
+++ ft_isalnum.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-carv <mde-carv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 16:01:15 by mde-carv          #+#    #+#             */
+/*   Updated: 2025/11/03 16:53:46 by mde-carv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* #include <stdio.h> */
+
+int	ft_isalnum(int c)
+{
+	if (!((c > 64 && c < 91) || (c > 96 && c < 123)
+			|| (c > 47 && c < 58)))
+		return (0);
+	else
+		return (1);
+}
+/* int main(void)
+{
+	printf("%d", ft_isalnum('0'));
+	printf("%c", '\n');
+	printf("%d", ft_isalnum('a'));
+	printf("%c", '\n');
+	printf("%d", ft_isalnum('A'));
+	printf("%c", '\n');
+	printf("%d", ft_isalnum('\n'));
+} */
